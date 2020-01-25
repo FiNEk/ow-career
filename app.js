3218926b7a6b6756c7861c52ff0dad140ad740b7
Denis Anashkin <fnk007@yandex.ru>
2020-01-25 12:33:01 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Overwatch career app')
+});
+
+app.listen(() => console.log(`App running on port ${PORT}`));
