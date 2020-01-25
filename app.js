7af2d10be04cd882861a5ed3fdcbfd92872fa9a5
Anashkin Denis <fnk007@yandex.ru>
2020-01-25 14:12:55 +0300

    .

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Overwatch career app')
 });
 
