f9f01c880337ea3635a7db18d0fa610996ede68e
Anashkin Denis <fnk007@yandex.ru>
2020-02-10 13:22:17 +0300

    check for pg password env var

--- src/startup/config.ts
+++ src/startup/config.ts
@@ -3,7 +3,12 @@ import config from "config";
 export default function() {
   if (!config.get("jwtPrivateKey")) {
     throw new Error(
-      "FATAL ERROR - ENVIRONMENT VARIABLE wnm_jwtPrivateKey IS NOT DEFINED"
+      "FATAL ERROR - ENVIRONMENT VARIABLE owcareer_jwtPrivateKey IS NOT DEFINED"
+    );
+  }
+  if (!config.get("postgresPswd")) {
+    throw new Error(
+      "FATAL ERROR - ENVIRONMENT VARIABLE owcareer_pgPassword IS NOT DEFINED"
     );
   }
   const port = config.get("defaultPort");
